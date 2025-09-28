26f6d73e7119699c9cd6174e020a44824ca2d0fe
Alisson Soares <alissonluka@gmail.com>
2025-09-28 06:18:11 +0000

    @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
-import { Playfair_Display, PT_Sans } from 'next/font/google';
+import { Playfair_Display, Inter } from 'next/font/google';
 import { cn } from '@/lib/utils';
 
 export const metadata: Metadata = {
@@ -15,9 +15,9 @@ const fontPlayfair = Playfair_Display({
   variable: '--font-headline',
 });
 
-const fontPtSans = PT_Sans({
+const fontInter = Inter({
   subsets: ['latin'],
-  weight: ['400', '700'],
+  weight: ['400', '500', '700'],
   variable: '--font-body',
 });
 
@@ -28,11 +28,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
+      <head>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,1,0" />
+      </head>
       <body
         className={cn(
           'antialiased',
           fontPlayfair.variable,
-          fontPtSans.variable
+          fontInter.variable
         )}
       >
         {children}
