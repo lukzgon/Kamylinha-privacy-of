fd40efabf11f5b9768019b9802c7c845d2134233
Alisson Soares <alissonluka@gmail.com>
2025-09-29 03:26:38 +0000

    o terceiro feed nao esta atualizando

--- src/components/home-page-content.tsx
+++ src/components/home-page-content.tsx
@@ -155,7 +155,7 @@ export function HomePageContent() {
   const feedPosts = [
     { id: 'popup-reset-card', src: 'https://i.postimg.cc/CK3x6Bc3/photo-2025-08-28-16-37-19.jpg', likes: 1248, comments: 126 },
     { id: undefined, src: 'https://i.postimg.cc/DwxfFbm0/kamy02.gif', likes: 2312, comments: 88 },
-    { id: undefined, src: 'https://i.postimg.cc/1tY8JPWf/post1-3-1-2.gif', likes: 889, comments: 45 },
+    { id: undefined, src: 'https://i.postimg.cc/d1G5nN1x/kamy03-4.gif', likes: 889, comments: 45 },
     { id: undefined, src: 'https://i.postimg.cc/4y8wXgHC/AYddvAdP.jpg', likes: 4402, comments: 150 },
     { id: undefined, src: 'https://i.postimg.cc/7Ld58QZb/kamy05-2.gif', likes: 3275, comments: 99 },
     { id: 'popup-trigger-card', src: 'https://i.postimg.cc/Y2fCbSYZ/photo-2025-08-28-15-09-54.jpg', likes: 4850, comments: 110 },
