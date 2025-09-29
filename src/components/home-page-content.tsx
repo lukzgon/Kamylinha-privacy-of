2de0fab175b2c1868a767430d0d97225d6a87b7d
Alisson Soares <alissonluka@gmail.com>
2025-09-29 03:04:11 +0000

    https://i.postimg.cc/6pVK543G/kamy03-4.gif troque esse fundo pela url qu

--- src/components/home-page-content.tsx
+++ src/components/home-page-content.tsx
@@ -153,9 +153,9 @@ export function HomePageContent() {
   const avatarImage = PlaceHolderImages.find(img => img.id === 'profile-avatar');
 
   const feedPosts = [
-    { id: 'popup-reset-card', src: 'https://i.postimg.cc/Pr2C4c2g/kamy01.jpg', likes: 1248, comments: 126 },
+    { id: 'popup-reset-card', src: 'https://i.postimg.cc/k4xPFFq7/kamy01.jpg', likes: 1248, comments: 126 },
     { id: undefined, src: 'https://i.postimg.cc/DwxfFbm0/kamy02.gif', likes: 2312, comments: 88 },
-    { id: undefined, src: 'https://i.postimg.cc/6pVK543G/kamy03-4.gif', likes: 889, comments: 45 },
+    { id: undefined, src: 'https://i.postimg.cc/Pr2C4c2g/kamy01.jpg', likes: 889, comments: 45 },
     { id: undefined, src: 'https://i.postimg.cc/4y8wXgHC/AYddvAdP.jpg', likes: 4402, comments: 150 },
     { id: undefined, src: 'https://i.postimg.cc/7Ld58QZb/kamy05-2.gif', likes: 3275, comments: 99 },
     { id: 'popup-trigger-card', src: 'https://i.postimg.cc/Y2fCbSYZ/photo-2025-08-28-15-09-54.jpg', likes: 4850, comments: 110 },
@@ -268,5 +268,7 @@ Escolha um dos planos abaixo e libere seu acesso agora mesmo! 😈
     </>
   );
 }
+
+    
 
     
