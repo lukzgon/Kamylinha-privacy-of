002281b5e10e7fbcf5d7af44a39ce4d55c939d34
Alisson Soares <alissonluka@gmail.com>
2025-09-30 00:42:48 +0000

    resolva o problema da primeira midia do primeiro card e da terceira midi

--- src/components/home-page-content.tsx
+++ src/components/home-page-content.tsx
@@ -160,17 +160,16 @@ export function HomePageContent() {
 
   useEffect(() => {
     if (bioRef.current) {
-      // Check if the content is taller than the container's initial max-height
-      if (bioRef.current.scrollHeight > 100) {
+      if (bioRef.current.scrollHeight > bioRef.current.clientHeight) {
         setShowReadMore(true);
       }
     }
   }, [bioText]);
 
   const feedPosts = [
-    { id: 'popup-reset-card', src: 'https://i.postimg.cc/W34k1R9b/photo-2025-08-28-16-37-19-3.jpg', likes: 1248, comments: 126 },
+    { id: 'popup-reset-card', src: 'https://i.postimg.cc/CK3x6Bc3/photo-2025-08-28-16-37-19.jpg', likes: 1248, comments: 126 },
     { id: undefined, src: 'https://i.postimg.cc/DwxfFbm0/kamy02.gif', likes: 2312, comments: 88 },
-    { id: undefined, src: 'https://i.postimg.cc/t96qgL35/kamy03.gif', likes: 889, comments: 45 },
+    { id: undefined, src: 'https://i.postimg.cc/1tY8JPWf/post1-3-1-2.gif', likes: 889, comments: 45 },
     { id: undefined, src: 'https://i.postimg.cc/4y8wXgHC/AYddvAdP.jpg', likes: 4402, comments: 150 },
     { id: undefined, src: 'https://i.postimg.cc/7Ld58QZb/kamy05-2.gif', likes: 3275, comments: 99 },
     { id: 'popup-trigger-card', src: 'https://i.postimg.cc/Y2fCbSYZ/photo-2025-08-28-15-09-54.jpg', likes: 4850, comments: 110 },
