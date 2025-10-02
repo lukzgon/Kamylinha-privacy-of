0627426b6c05d88cf029e5b3acfc6a59a6b9e892
Alisson Soares <alissonluka@gmail.com>
2025-10-02 16:44:16 +0000

    mude para 17,90 (_for element strong_)

--- src/components/home-page-content.tsx
+++ src/components/home-page-content.tsx
@@ -171,9 +171,8 @@ export function HomePageContent() {
   const feedPosts = [
     { id: 'popup-reset-card', src: 'https://i.postimg.cc/CK3x6Bc3/photo-2025-08-28-16-37-19.jpg', likes: 1248, comments: 126 },
     { id: undefined, src: 'https://i.postimg.cc/DwxfFbm0/kamy02.gif', likes: 2312, comments: 88 },
-    { id: undefined, src: 'https://i.postimg.cc/4y8wXgHC/AYddvAdP.jpg', likes: 4402, comments: 150 },
-    { id: undefined, src: 'https://i.postimg.cc/7Ld58QZb/kamy05-2.gif', likes: 3275, comments: 99 },
     { id: 'popup-trigger-card', src: 'https://i.postimg.cc/Y2fCbSYZ/photo-2025-08-28-15-09-54.jpg', likes: 4850, comments: 110 },
+    { id: undefined, src: 'https://i.postimg.cc/7Ld58QZb/kamy05-2.gif', likes: 3275, comments: 99 },
   ];
   
   const handleMediaClick = () => {
@@ -254,7 +253,7 @@ export function HomePageContent() {
             <div className="mt-4 flex flex-col gap-3">
                <Plan
                 duration="1 Mês"
-                price="R$ 29,90"
+                price="R$ 17,90"
                 oldPrice="R$ 49,90"
                 tag={{ text: 'ECONOMIA', className: 'economy-tag' }}
                 href="https://www.ggcheckout.com/checkout/v2/oT5Fw5yXz9g1d8C5P3Iq"
@@ -292,6 +291,8 @@ export function HomePageContent() {
     </>
   );
 }
+
+    
 
     
 
